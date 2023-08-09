b99803a857b916fc8a59dcba090b7713e6e7d187
iraprojects <kristenirazu@gmail.com>
2023-08-08 22:15:10 -0600

    created the TextArea component, assign the props and defined the handleChang

---
+++ src/components/TextArea.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { Form } from 'react-bootstrap'
+import { SectionType } from '../types.d'
+
+interface Props {
+  type: SectionType
+  placeholder: string
+  loading?: boolean
+  onChange: (value: string) => void
+  value: string
+}
+
+const commonStyles = { border: 0, height: '200px', resize: 'none' as 'none' }
+
+const getPlaceHolder = ({type, loading}: {type: SectionType, loading?: boolean}) => {
+  if (type === SectionType.From) return 'Introducir texto'
+  if (loading === true) return 'Cargando...'
+  return 'Traduccion'
+} 
+
+function TextArea({ type, loading, onChange, value }: Props) {
+  const styles = type === SectionType.From
+    ? commonStyles
+    : { ...commonStyles, backgroundColor: '#f5f5f5' }
+
+  const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    onChange(event.target.value)
+  }
+
+  return (
+    <Form.Control
+      autoFocus={type === SectionType.From}
+      as='textarea'
+      placeholder={getPlaceHolder({type, loading})}
+      style={styles}
+      value={value}
+      onChange={handleChange}
+    />
+  )
+}
+
+export default TextArea
