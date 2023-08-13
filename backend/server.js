b6618270478e39792f251c850f4052b63e36f988
iraprojects <kristenirazu@gmail.com>
2023-08-13 17:33:15 -0600

    added a condition to add the sourceLang in case it is different from auto

--- backend/server.js
+++ backend/server.js
@@ -9,24 +9,29 @@ app.use(cors())
 const DEEPL_API_KEY = process.env.DEEPL_API_KEY
 
 app.post('/translate', async (req, res) => {
-  const { text, targetLang } = req.body
-  const deeplUrl = `https://api-free.deepl.com/v2/translate?auth_key=${DEEPL_API_KEY}&text=${encodeURIComponent(text)}&target_lang=${targetLang}`
+  const { text, targetLang, sourceLang } = req.body;
 
   try {
-    const response = await fetch(deeplUrl)
-    if (response.ok) {
-      const data = await response.json()
-      res.json({
-        translation: data.translations[0].text
-      })
-    } else {
-      res.status(response.status).json({ error: response.statusText })
-    }
-  } catch (error) {
-    res.status(500).json({ error: 'Translation Failed' })
+    const response = await fetch('https://api-free.deepl.com/v2/translate', {
+      method: 'POST',
+      headers: {
+        'Authorization': `DeepL-Auth-Key ${DEEPL_API_KEY}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        text: [text],
+        target_lang: targetLang,
+        ...(sourceLang !== 'auto' ? {source_lang: sourceLang} : {})
+      }),
+    });
 
+    const data = await response.json();
+    const translation = data.translations[0].text;
+    res.json({ translation });
+  } catch (error) {
+    res.status(500).json({ error: 'Translation failed' });
   }
-})
+});
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
