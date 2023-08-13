dbf0a71479d2f6f24c76107bd7bc5f3ecf990970
iraprojects <kristenirazu@gmail.com>
2023-08-13 17:27:09 -0600

    created a separate file for the connection to the api in the backend and added useEffect

---
+++ src/hooks/useTranslationEffect.ts
@@ -0,0 +1,50 @@
+import { useEffect, useState } from 'react';
+import { Language, AutoLanguage } from '../types.d';
+
+export function useTranslationEffect(
+  fromText: string,
+  toLanguage: Language,
+  fromLanguage: Language | AutoLanguage,
+  setResult: (payload: string) => void
+){
+  const [debouncedFromText, setDebouncedFromText] = useState('')
+
+  useEffect(() => {
+    const timeOutId = setTimeout(() => {
+      setDebouncedFromText(fromText)
+    }, 500)
+
+    return () => {
+      clearTimeout(timeOutId)
+    }
+
+  },[fromText])
+
+  useEffect(() => {
+    
+    const translateText = async () => {
+      try {
+        const response = await fetch('http://localhost:5000/translate', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            text: fromText,
+            targetLang: toLanguage,
+            ...(fromLanguage !== 'auto' ? { sourceLang: fromLanguage } : {})
+          }),
+        });
+  
+        const data = await response.json();
+        setResult(data.translation);
+        console.log(fromLanguage);
+        
+      } catch (error) {
+        console.error('Translation error:', error);
+      }
+    };
+    translateText()
+    
+  },[debouncedFromText, fromLanguage, toLanguage])
+}
