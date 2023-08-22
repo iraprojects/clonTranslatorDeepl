fb73ea8bf742424dbee5efbd77c9ad2ede3e1c80
iraprojects <kristenirazu@gmail.com>
2023-08-21 22:35:25 -0600

    deploy config

--- src/hooks/useTranslationEffect.ts
+++ src/hooks/useTranslationEffect.ts
@@ -6,45 +6,43 @@ export function useTranslationEffect(
   toLanguage: Language,
   fromLanguage: Language | AutoLanguage,
   setResult: (payload: string) => void
-){
-  const [debouncedFromText, setDebouncedFromText] = useState('')
+) {
+  const [debouncedFromText, setDebouncedFromText] = useState('');
 
   useEffect(() => {
     const timeOutId = setTimeout(() => {
-      setDebouncedFromText(fromText)
-    }, 500)
+      setDebouncedFromText(fromText);
+    }, 500);
 
     return () => {
-      clearTimeout(timeOutId)
-    }
-
-  },[fromText])
+      clearTimeout(timeOutId);
+    };
+  }, [fromText]);
 
   useEffect(() => {
-    
     const translateText = async () => {
       try {
-        const response = await fetch('https://api-translator-deelp-backend.onrender.com/translate', {
+        const apiKey = process.env.NEXT_PUBLIC_REACT_APP_DEEPL_API_KEY;
+
+        const response = await fetch('https://api-free.deepl.com/v2/translate', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
+            'Authorization': `DeepL-Auth-Key ${apiKey}`,
           },
           body: JSON.stringify({
             text: debouncedFromText,
-            targetLang: toLanguage,
-            ...(fromLanguage !== 'auto' ? { sourceLang: fromLanguage } : {})
+            target_lang: toLanguage,
+            ...(fromLanguage !== 'auto' ? { source_lang: fromLanguage } : {}),
           }),
         });
-  
+
         const data = await response.json();
-        setResult(data.translation);
-        console.log(fromLanguage);
-        
+        setResult(data.translations[0].text);
       } catch (error) {
         console.error('Translation error:', error);
       }
     };
     translateText()
-    
-  },[debouncedFromText, fromLanguage, toLanguage])
+  }, [debouncedFromText, fromLanguage, toLanguage]);
 }
